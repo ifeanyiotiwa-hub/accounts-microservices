980a3989a3575213cc755952a8aae92b782a6e10
Ifeanyichukwu Otiwa <IFEANYICHUKWU OTIWA>
2022-03-14 22:11:29 +0100

    modifies AccountsController.java

--- src/main/java/org/betpawa/accounts/controller/AccountsController.java
+++ src/main/java/org/betpawa/accounts/controller/AccountsController.java
@@ -5,6 +5,7 @@ import org.betpawa.accounts.model.Accounts;
 import org.betpawa.accounts.model.Customer;
 import org.betpawa.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +17,13 @@ public class AccountsController {
 
     private final AccountsRepository accountsRepository;
 
-    @Autowired
     public AccountsController(AccountsRepository accountsRepository) {
         this.accountsRepository = accountsRepository;
     }
 
 
     @PostMapping("/accounts")
-    public Accounts getAccounts(@RequestBody Customer customer) {
+    public Accounts getAccountsDetails(@RequestBody Customer customer) {
         return accountsRepository.findByCustomerId(customer.getCustomerId());
     }
 }
