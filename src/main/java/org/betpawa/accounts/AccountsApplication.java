acd93f200475443b60ccdf60a9c7b201ec585afb
Ifeanyichukwu Otiwa <IFEANYICHUKWU OTIWA>
2022-03-14 07:42:25 +0100

    modifies AccountsApplication.java

--- src/main/java/org/betpawa/accounts/AccountsApplication.java
+++ src/main/java/org/betpawa/accounts/AccountsApplication.java
@@ -2,8 +2,15 @@ package org.betpawa.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScans({@ComponentScan("org.betpawa.accounts.controller")})
+@EnableJpaRepositories("org.betpawa.accounts.repository")
+@EntityScan("org.betpawa.accounts.model")
 public class AccountsApplication {
 
     public static void main(String[] args) {
