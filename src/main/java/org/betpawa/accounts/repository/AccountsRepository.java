4803984c5775b46ebe9c633d1cb0288ee5160235
Ifeanyichukwu Otiwa <IFEANYICHUKWU OTIWA>
2022-03-14 22:11:02 +0100

    modifies AccountsRepository.java

--- src/main/java/org/betpawa/accounts/repository/AccountsRepository.java
+++ src/main/java/org/betpawa/accounts/repository/AccountsRepository.java
@@ -1,10 +1,11 @@
 package org.betpawa.accounts.repository;
 
 import org.betpawa.accounts.model.Accounts;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountsRepository  extends JpaRepository<Accounts, Integer> {
+public interface AccountsRepository  extends CrudRepository<Accounts, Long> {
     Accounts findByCustomerId(int customerId);
 }
