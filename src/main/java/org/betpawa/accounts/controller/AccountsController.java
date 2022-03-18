c9b047be57f023ca8a7ce21163bfc11cd25a9a84
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-03-18 12:46:39 +0100

    modifies AccountsController.java file

--- src/main/java/org/betpawa/accounts/controller/AccountsController.java
+++ src/main/java/org/betpawa/accounts/controller/AccountsController.java
@@ -1,14 +1,20 @@
 package org.betpawa.accounts.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.betpawa.accounts.model.Accounts;
 import org.betpawa.accounts.model.Customer;
+import org.betpawa.accounts.model.Properties;
 import org.betpawa.accounts.repository.AccountsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.betpawa.accounts.services.AccountsServiceConfig;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class AccountsController {
@@ -17,13 +23,28 @@ public class AccountsController {
 
     private final AccountsRepository accountsRepository;
 
-    public AccountsController(AccountsRepository accountsRepository) {
+    private final AccountsServiceConfig accountServiceConfig;
+
+    public AccountsController(AccountsRepository accountsRepository, AccountsServiceConfig accountServiceConfig) {
         this.accountsRepository = accountsRepository;
+        this.accountServiceConfig = accountServiceConfig;
     }
 
 
     @PostMapping("/myAccounts")
     public Accounts getAccountsDetails(@RequestBody Customer customer) {
         return accountsRepository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("/accounts/properties")
+    public ResponseEntity<Properties> getPropertiesDetails() throws JsonProcessingException {
+//        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties.AccountsPropertiesBuilder().msg(accountServiceConfig.getMsg())
+                .buildVersion(accountServiceConfig.getBuildVersion())
+                .mailDetails(accountServiceConfig.getMailDetails())
+                .activeBranches(accountServiceConfig.getActiveBranches())
+                .build();
+
+        return ResponseEntity.ok().body(properties);
     }
 }
