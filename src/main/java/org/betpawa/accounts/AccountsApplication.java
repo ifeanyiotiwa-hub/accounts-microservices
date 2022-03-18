f4d879a462739727cd7a6a206cf4c935b5aa366a
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-03-18 12:51:48 +0100

    renamed AccountsApplication

--- src/main/java/org/betpawa/accounts/AccountsApplication.java
+++ src/main/java/org/betpawa/accounts/AccountsApplication.java
@@ -3,12 +3,14 @@ package org.betpawa.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScans({@ComponentScan("org.betpawa.accounts.controller")})
+@RefreshScope
 @EnableJpaRepositories("org.betpawa.accounts.repository")
 @EntityScan("org.betpawa.accounts.model")
 public class AccountsApplication {
